02f9fe16413545bebb09c411aff0578b75de1032
Emin <106148850+Emin-1337@users.noreply.github.com>
2022-07-04 12:50:37 +0900

    [Bronze V] Title: A+B - 4, Time: 80 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (str == null) 
+                    break;
+                string[] str2 = str.Split();
+                Console.WriteLine(int.Parse(str2[0]) + int.Parse(str2[1]));
+            }
+        }
+    }
+}
