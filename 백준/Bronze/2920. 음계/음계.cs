00203250b1e3d1b4622daf08d5261ebb0c902168
Emin-1337 <106148850+Emin-1337@users.noreply.github.com>
2022-07-04 11:02:28 +0900

    [Bronze II] Title: 음계, Time: 60 ms, Memory: 5356 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cs
@@ -0,0 +1,7 @@
+string str = Console.ReadLine();
+            if (str == "1 2 3 4 5 6 7 8")
+                Console.WriteLine("ascending");
+            else if (str == "8 7 6 5 4 3 2 1")
+                Console.WriteLine("descending");
+            else
+                Console.WriteLine("mixed");
