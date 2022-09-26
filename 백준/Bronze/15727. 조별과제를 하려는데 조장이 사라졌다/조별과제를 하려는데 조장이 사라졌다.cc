d3c6aea9610d9bfad870028a4762c65da75191c4
이상민 <106148850+Emin137@users.noreply.github.com>
2022-09-27 00:47:25 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main()
+{
+	int a;
+	cin >> a;
+	if (a % 5 == 0)
+		cout << a / 5;
+	else
+		cout << a / 5 + 1;
+}
+	
+
+
