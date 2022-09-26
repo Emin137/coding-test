2c23ca027c5bdc2337dd8178e6258daddc31d08d
이상민 <106148850+Emin137@users.noreply.github.com>
2022-09-26 23:41:18 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	for (size_t i = 0; i < a; i++)
+	{
+		int c, d;
+		cin >> c >> d;
+	}
+	cout << "비와이";
+}
+
+
