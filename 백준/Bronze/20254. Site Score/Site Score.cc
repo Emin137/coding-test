e3a094248d51dbdd26e8e19d5c3bb5ec0862385d
이상민 <106148850+Emin137@users.noreply.github.com>
2022-09-27 22:00:39 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << 56 * a + 24 * b + 14 * c + 6 * d;
+}
+	
+
+
