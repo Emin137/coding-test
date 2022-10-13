b9adc69671c52869158d6f39b1e921e6ae13ba66
이상민 <106148850+Emin137@users.noreply.github.com>
2022-10-13 17:55:02 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	if (a == 1)
+	{
+		cout << "SciComLove";
+	}
+	else
+	{
+		cout << "SciComLove" << endl << "SciComLove";
+	}
+
+}
