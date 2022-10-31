6fb1fdfa5ca7db7c6612465b5f2c363c854baf0c
이상민 <106148850+Emin137@users.noreply.github.com>
2022-10-31 12:49:48 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	a = a * -b;
+	int c, d, e, f, g;
+	cin >> c >> d >> e >> f >> g;
+	cout << c + a << ' ' << d + a << ' ' << e + a << ' ' << f + a << ' ' << g + a;
+	
+}
+	
+
+
