9409022241acab62099aa33c6442c3a43734d2c6
이상민 <106148850+Emin137@users.noreply.github.com>
2022-11-01 13:08:28 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main()
+{
+	while (1)
+	{
+		int a, b;
+		cin >> a >> b;
+		if (!a && !b)
+			break;
+		cout << a + b << endl;
+	}
+}
+	
+
+
