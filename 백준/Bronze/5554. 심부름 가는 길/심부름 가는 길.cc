b767689a943d532742c072c04b607adcd8c64a68
이상민 <106148850+Emin137@users.noreply.github.com>
2022-11-01 12:57:52 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main()
+{
+	int a, b, c, d;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cin >> d;
+	cout << (a + b + c + d) / 60 << endl;
+	cout << (a + b + c + d) % 60 << endl;
+}
+	
+
+
