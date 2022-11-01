c9e2959c35d843d5d191968cbb08331d7cc1a132
이상민 <106148850+Emin137@users.noreply.github.com>
2022-11-01 13:07:14 +0900

    [Bronze IV] Title: 시험 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main()
+{
+	int a, b, c, d, e, f, g, h;
+	cin >> a >> b >> c >> d;
+	cin >> e >> f >> g >> h;
+	cout << max(a + b + c + d, e + f + g + h);
+}
+	
+
+
