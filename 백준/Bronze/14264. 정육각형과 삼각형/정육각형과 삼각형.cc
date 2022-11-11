ba112b9e144717c2821c30818fa6dc48568ce56c
이상민 <106148850+Emin137@users.noreply.github.com>
2022-11-11 11:26:18 +0900

    [Bronze IV] Title: 정육각형과 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14264. 정육각형과 삼각형/정육각형과 삼각형.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <iomanip>
+ using namespace std;
+int main()
+{
+	double a;
+	cin >> a;
+	cout << setprecision(25) << a * a / 4 * sqrt(3);
+	
+}
+	
+
+
