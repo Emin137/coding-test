bf7f87d587e6aeaefdf3ea6de8f6870aa68109bb
이상민 <106148850+Emin137@users.noreply.github.com>
2023-01-31 11:39:30 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <iomanip>
+ using namespace std;
+int main()
+{
+	string str;
+	cin >> str;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << str << "::fan:\n";
+	cout << ":fan::fan::fan:\n";
+}
+	
+
+
