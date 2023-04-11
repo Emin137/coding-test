d67eac6ee3826694e1ca1c861cac0acb4ed8f36b
Emin <106148850+Emin137@users.noreply.github.com>
2023-04-11 14:27:41 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin >> a >> b;
+    cin >> c;
+    b+=c;
+    if(b>=60)
+    {
+        a += b/60;
+        b = b%60;
+    }
+    if(a>=24)
+    {
+        a = a%24;
+    }
+    
+    cout << a << ' ' << b;
+        
+}
