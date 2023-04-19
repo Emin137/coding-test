7bc1768bf55862a927b55cb6b2f00060f6d17a43
Em2n <106148850+Em2n@users.noreply.github.com>
2023-04-19 16:36:59 +0900

    [Bronze II] Title: 하얀 칸, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int cnt=0;
+    for(int i=0; i<8; i++)
+    {
+        string str;
+        cin >> str;
+        if(i%2==0)
+        {
+            for(int j=0; j<8; j+=2)
+                if(str[j]=='F')
+                    cnt++;
+        }
+        else
+        {
+            for(int j=1; j<8; j+=2)
+                if(str[j]=='F')
+                    cnt++;
+        }
+    }
+    cout << cnt;
+    
+}
+
