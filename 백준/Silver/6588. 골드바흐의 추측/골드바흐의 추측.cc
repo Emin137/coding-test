33f864c847dabdec78d92f620f8279c2292aa3be
이상민 <106148850+Em2n@users.noreply.github.com>
2023-04-25 19:38:30 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 72 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.cc
@@ -2,10 +2,9 @@
     using namespace std;
     bool isPrime[1000001];
     int main()
-    {
+{
         cin.tie(NULL);
         ios::sync_with_stdio(0);
-        
         for(int i=2; i*i <= 1000001; i++)
         {
             if(isPrime[i]) continue;
@@ -19,20 +18,16 @@
             if(!n)
                 break;
             bool isFind=false;
-            int tempIdx = 0;
             for(int i=3; i < n/2+1; i+=2)
             {
-               if(!isPrime[i] && !isPrime[n-i])
-                   {
-                       tempIdx=i;
-                       isFind=true;
-                       break;
-                   }
+                if(!isPrime[i] && !isPrime[n-i])
+                {
+                    cout << n << " = " << i << " + " << n-i << '\n';
+                    isFind=true;
+                    break;
+                }
             }
-            if(isFind)
-                cout << n << " = " << tempIdx << " + " << n-tempIdx << '\n';
-            else
+            if(!isFind)
                 cout << "Goldbach's conjecture is wrong.\n";
         }
     }
-
