936b7c99f1ef35bcdc85876721c98a7a4940be9d
이상민 <106148850+Em2n@users.noreply.github.com>
2023-05-12 00:58:22 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> str_list, string ex) {
+    string answer = "";
+    for(auto &a : str_list)
+    {
+        if(a.find(ex) == string::npos)
+            answer+=a;
+    }
+    return answer;
+}
