b076af77a57adfe1f7d4b91d7fcd883be82d0ccb
이상민 <106148850+Em2n@users.noreply.github.com>
2023-05-12 12:41:01 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num, int k) {
+    int answer = 0;
+    string str = to_string(num);
+    for(int i=0;i<str.size();i++)
+    {
+        if(str[i]-48 == k)
+            return i+1;
+    }
+    return -1;
+}
