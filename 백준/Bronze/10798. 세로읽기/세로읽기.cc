957b5c43bbc30f859e9bb34801679cc45f605c05
이상민 <emin98090@naver.com>
2024-04-01 09:01:56 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+char str[5][16];
+
+int main(){
+    for(int i=0; i<5; i++){
+        scanf("%s", str[i]);
+    }
+
+    for(int i=0; i<16; i++){
+        for(int j=0; j<5; j++){
+            if(str[j][i])
+                printf("%c", str[j][i]);
+        }
+    }
+
+    return 0;
+}
