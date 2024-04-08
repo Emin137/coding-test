c5a7edc04d9257d8caaa7dc7ef875e9f49d0efac
이상민 <emin98090@naver.com>
2024-04-08 18:52:12 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    string str;
+    str = N == 0 ? "YONSEI" : "Leading the Way to the Future";
+    cout <<str;
+}
